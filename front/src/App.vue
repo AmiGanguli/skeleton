fb662caec9f0ee2cc3204019c51bc041632241e5
Ami Ganguli <ami.ganguli@gmail.com>
2020-05-19 08:09:36 -0400

    use vuetify google keep layout

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,60 +1,90 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+  <v-app id="main">
+    <v-app-bar app clipped-left color="amber">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <span class="title ml-3 mr-5">
+        Google&nbsp;
+        <span class="font-weight-light">Keep</span>
+      </span>
+      <v-text-field solo-inverted flat hide-details label="Search" prepend-inner-icon="mdi-magnify"></v-text-field>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
+      <v-list dense class="grey lighten-4">
+        <template v-for="(item, i) in items">
+          <v-row v-if="item.heading" :key="i" align="center">
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-col>
+            <v-col cols="6" class="text-right">
+              <v-btn small text>edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider v-else-if="item.divider" :key="i" dark class="my-4"></v-divider>
+          <v-list-item v-else :key="i" link>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title class="grey--text">{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
+      <v-container fluid class="grey lighten-4 fill-height">
+        <v-row justify="center" align="center">
+          <v-col class="shrink">
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn :href="source" icon large target="_blank" v-on="on">
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+// Icon reference here:
+//    https://materialdesignicons.com/
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  props: {
+    source: String
   },
-
   data: () => ({
-    //
-  }),
+    drawer: null,
+    items: [
+      { icon: "mdi-notebook", text: "Notes" },
+      { icon: "mdi-reminder", text: "Reminders" },
+      { divider: true },
+      { heading: "Labels" },
+      { icon: "mdi-plus-thick", text: "Create new label" },
+      { divider: true },
+      { icon: "mdi-archive", text: "Archive" },
+      { icon: "mdi-delete", text: "Trash" },
+      { divider: true },
+      { icon: "mdi-cog", text: "Settings" },
+      { icon: "mdi-delete", text: "Trash" },
+      { icon: "mdi-help-circle", text: "Help" },
+      { icon: "mdi-download", text: "App downloads" },
+      { icon: "mdi-keyboard", text: "Keyboard shortcuts" }
+    ]
+  })
 };
 </script>
+
+<style>
+#main .v-navigation-drawer__border {
+  display: none;
+}
+</style>
